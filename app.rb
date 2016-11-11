edc0c9c5e7dcada8e8c87329ebd881a64ee66d81
Anton <kokhanchik.tversu@gmail.com>
2016-11-11 10:23:58 +0300

    trouble fixed

--- app.rb
+++ app.rb
@@ -36,20 +36,23 @@ DB.create_table :students do
     foreign_key :city_id, :cities, :null=>false
 end
 
-DB[:cities].import ([:city_name, :city_country], [
-    ['Erfurt','Germany'],
 students = DB[:students]
 cities = DB[:cities]
+
+cities.import(
+  [:city_name, :city_country],
+  [ ['Erfurt','Germany'],
     ['San-Francisco', 'USA'],
     ['Capetown', 'South Africa'],
     ['Beijing(Pekin)', 'China'],
     ['Essen', 'Germany'],
     ['Hamburg', 'Germany'],
-    ['Atlanta', 'USA']
-    ])
+    ['Atlanta', 'USA'] ] )
 
-DB[:students].import([:name,:surname, :course, :is_male, :city_id], [
-    ['Mark', 'Schmidt', 3,true, 1],
+
+students.import(
+  [:name,:surname, :course, :is_male, :city_id],
+  [ ['Mark', 'Schmidt', 3,true, 1],
     ['Helen', 'Hunt', 2, false, 2],
     ['Matumba', 'Zuko', 4, true, 3],
     ['Rin', 'Kupo', 4, false,3],
@@ -57,6 +60,4 @@ DB[:students].import([:name,:surname, :course, :is_male, :city_id], [
     ['Peter', 'Zimmer', 3, true, 5],
     ['Hanz', 'Mueller', 4, true, 6],
     ['Alisa', 'Kepler', 4, false, 1],
-    ['Anna', 'Madavie', 2, false, 7]
-    ])
-# FIXME: syntax error, unexpected ',', expecting ')' in 40 line
+    ['Anna', 'Madavie', 2, false, 7] ] )
